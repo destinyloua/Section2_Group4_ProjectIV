c2f8c6d00874e1dfeb1ca73a98d3eed7755b9a96
TylerDdao <baonamdao05@gmail.com>
2025-03-14 16:11:51 -0400

    update

--- Server/Server/src/leaf_laugh_love/Leaf_laugh_love.java
+++ Server/Server/src/leaf_laugh_love/Leaf_laugh_love.java
@@ -59,7 +59,7 @@ public class Leaf_laugh_love extends JFrame {
 		String url = "jdbc:mysql://localhost:3306/project4";
 		//Change these!!
         String user = "root";
-        String password = "Lolecksdee2/";
+        String password = "Tyler123";
 		setTitle("Leaf, Laugh, Love Dashboard");
 		DatabaseHandler.SetUpConnection(url, user, password);
 		DatabaseHandler.ConnectDatabase();
